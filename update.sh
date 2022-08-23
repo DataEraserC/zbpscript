9f18d9504ff2e8ba977ff1631dc9625b4dea92f8
DataEraser <MayuriNFC@outlook.com>
2022-08-23 11:03:26 +0000

    update script so that i could use it

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 echo "Updateing"
 rm  -rf ~/qqbot/data/{leveldb-v2,Reborn,SetuTime,videos,images,VtbQuotation,sleep,hs,voices,cache,acgimage,Funny}
-wget -O ~/qqbot/qqbot.sh https://raw.githubusercontent.com/MayuriNFC/zbpscript/main/qqbot.sh
-wget -O ~/qqbot/gocq.tar.gz https://github.com/Mrs4s/go-cqhttp/releases/latest/download/go-cqhttp_linux_amd64.tar.gz
-wget -O ~/qqbot/zbp.tar.gz https://github.com/opensourcefuture/ZeroBot-Plugin/releases/latest/download/zbp_linux_amd64.tar.gz
+wget -O ~/qqbot/qqbot.sh https://raw.githubusercontent.com/DataEraserC/zbpscript/main/qqbot.sh
+wget -O ~/qqbot/gocq.tar.gz https://github.com/Mrs4s/go-cqhttp/releases/latest/download/go-cqhttp_linux_arm64.tar.gz
+wget -O ~/qqbot/zbp.tar.gz https://github.com/DataEraserC/ZeroBot-Plugin/releases/latest/download/zbp_linux_arm64.tar.gz
 tar -xzvf ~/qqbot/gocq.tar.gz -C ~/qqbot/
 tar -xzvf ~/qqbot/zbp.tar.gz  -C ~/qqbot/
 rm ~/qqbot/gocq.tar.gz  ~/qqbot/zbp.tar.gz ~/qqbot/LICENSE ~/qqbot/README.md
