45e95b12c5fda05556abac92654c9d9602e50c8f
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2022-09-30 12:36:04 +0800

    Update zbp.sh

--- zbp.sh
+++ zbp.sh
@@ -1,5 +1,6 @@
 cd ~/qqbot/
 rm  -rf ~/qqbot/data/{leveldb-v2,Reborn,videos,images,voices,cache,Funny}
 TZ='Asia/Shanghai'; export TZ
+LC_ALL="zh_CN.utf-8"; export TZ
 ./zbp -d -p ">" -c config.json
 sh ~/qqbot/zbp.sh
