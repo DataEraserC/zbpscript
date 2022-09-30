48af64703d81e8ee97fed6c53796ad59dd783700
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2022-09-30 12:36:17 +0800

    Update gocq.sh

--- gocq.sh
+++ gocq.sh
@@ -1,5 +1,6 @@
 cd ~/qqbot
 rm  -rf ~/qqbot/data/{leveldb-v2,Reborn,videos,images,voices,cache,Funny}
 TZ='Asia/Shanghai'; export TZ
+LC_ALL="zh_CN.utf-8"; export TZ
 ./go-cqhttp faststart
 sh ~/qqbot/gocq.sh
